8cc0dc96e2d15fab6f904194725ed9650d51a404
Dmytro-Kobets <kobets.dmytro9@gmail.com>
2023-06-30 19:34:56 +0300

    final

--- utils/contacsValidators.js
+++ utils/contacsValidators.js
@@ -4,20 +4,18 @@ exports.createContactValidator = (data) =>
   Joi.object()
     .keys({
       name: Joi.string().min(3).max(20).required(),
-      email: Joi.string().min(3).max(20).required(),
+      email: Joi.string().email().required(),
       phone: Joi.string()
         .regex(/^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$/)
         .required(),
     })
     .validate(data);
 
-exports.updateContactValidator = (data) =>
+exports.favoriteValidator = (data) =>
   Joi.object()
     .keys({
-      name: Joi.string().min(3).max(20),
-      email: Joi.string().min(3).max(20),
-      phone: Joi.string().regex(
-        /^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$/
-      ),
+      favorite: Joi.boolean().required().messages({
+        "any.required": "Missing field favorite",
+      }),
     })
     .validate(data);
